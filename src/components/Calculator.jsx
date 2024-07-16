311bbcfa3edef3f3484abc59425423ba6e190d19
Yacine Haddouche <100038941+gityacine@users.noreply.github.com>
2024-07-16 11:36:16 +0100

    got rid of App component, switched to createBrowserRouter, and did some refactoring

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Calculator() {
+    return (
+        <>
+            <h1>This is the Calculator route</h1>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from './components/Layout.jsx';
+import Home from './components/Home.jsx';
+import Calculator from './components/Calculator.jsx';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "calculator",
+        element: <Calculator />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
