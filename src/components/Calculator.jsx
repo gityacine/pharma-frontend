bfa3cb27043e6f157b54a8b2edb1a556acb9f6ec
Yacine Haddouche <100038941+gityacine@users.noreply.github.com>
2024-07-28 19:12:30 +0100

    finished calculator functionality

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -13,14 +13,43 @@ export async function loader() {
         product.allocated = "0";
     });
     const materials = await materialsRes.json();
-    const productMaterials = await productMaterialsRes.json();
-    return { productsData };
+    const productMaterialsData = await productMaterialsRes.json();
+    return { productsData, productMaterialsData, materials };
+}
+
+function getProductMaterials(id, productMaterials) {
+    for (const element of productMaterials) {
+        if (element.id === id) {
+            return element.materials;
+        }
+    }
+}
+
+function getMaterialData(id, materials, property) {
+    for (const material of materials) {
+        if (material.id === id) {
+            return material[property];
+        }
+    }
+}
+
+function getEntryIndex(entries, id) {
+    for (let i = 0; i < entries.length; i++) {
+        if (entries[i].id === id) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 export default function Calculator() {
-    const { productsData } = useLoaderData();
+    const { productsData, productMaterialsData, materials } = useLoaderData();
     const [products, setProducts] = useState(productsData);
+    const [entries, setEntries] = useState([]);
     const modalRef = useRef(null);
+    const downloadButtonRef = useRef(null);
+    const downloadApproButtonRef = useRef(null);
+    const urlRef = useRef(null);
 
     function allocate(id) {
         const nextProducts = products.map(product => {
@@ -66,19 +95,70 @@ export default function Calculator() {
 
     function openModal() {
         if (modalRef.current) {
+            const newEntries = [];
+            for (const product of products) {
+                if (parseInt(product.allocated) > 0) {
+                    const productMaterials = getProductMaterials(product.id, productMaterialsData);
+                    for (const productMaterial of productMaterials) {
+                        const entry = {
+                            id: productMaterial.id,
+                            name: getMaterialData(productMaterial.id, materials, "name"),
+                            stock: getMaterialData(productMaterial.id, materials, "quantity"),
+                            quantity: parseInt(product.allocated) * productMaterial.coeff,
+                        };
+                        const index = getEntryIndex(newEntries, entry.id);
+                        if (index === -1) {
+                            newEntries.push(entry);
+                        } else {
+                            newEntries[index].quantity = newEntries[index].quantity + entry.quantity;
+                        }
+                    }
+                }
+            }
+            let csvContent = newEntries.map(entry => {
+                return `${entry.id},"${entry.name}",${entry.quantity},${entry.stock},${(entry.stock - entry.quantity) >= 0 ? "-" : (entry.quantity - entry.stock)}`
+            }).join("\n");
+            csvContent = "id,name,required,stock,difference\n" + csvContent;
+
+            let csvAppro = newEntries.filter(entry => {
+                return entry.quantity > entry.stock;
+            }).map(entry => {
+                return `${entry.id},"${entry.name}",${entry.quantity},${entry.stock},${(entry.stock - entry.quantity) >= 0 ? "-" : (entry.quantity - entry.stock)}`
+            }).join("\n");
+            csvAppro = "id,name,required,stock,difference\n" + csvAppro;
+
+            const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+            const url = URL.createObjectURL(blob);
+
+            const blobAppro = new Blob([csvAppro], { type: "text/csv;charset=utf-8;" });
+            const urlAppro = URL.createObjectURL(blobAppro);
+
+            urlRef.current = url;
+            if (downloadButtonRef.current) {
+                downloadButtonRef.current.href = url;
+                downloadButtonRef.current.download = "data.csv";
+            }
+
+            if (downloadApproButtonRef.current) {
+                downloadApproButtonRef.current.href = urlAppro;
+                downloadApproButtonRef.current.download = "data-appro.csv";
+            }
+
+            setEntries(newEntries);
             modalRef.current.showModal();
         }
     }
 
     function closeModal() {
-        if (modalRef.current) {
+        if (modalRef.current && urlRef.current) {
+            URL.revokeObjectURL(urlRef.current)
             modalRef.current.close();
         }
     }
 
     return (
         <>
-            <h1>This is the Calculator route</h1>
+            <h1>Calculator</h1>
             <table>
                 <thead>
                     <tr>
@@ -108,9 +188,32 @@ export default function Calculator() {
             <button
                 onClick={openModal}
             >Calculate</button>
-            <dialog ref={modalRef}>
-                <h2>Dialog Title</h2>
-                <p>This is a dialog content.</p>
+            <dialog ref={modalRef} onClose={() => { console.log("dialog closed"); }}>
+                <h2>Output</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Required</th>
+                            <th>Stock</th>
+                            <th>Difference</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {entries.map((entry) => (
+                            <tr key={entry.id}>
+                                <td>{entry.id}</td>
+                                <td>{entry.name}</td>
+                                <td>{entry.quantity}</td>
+                                <td>{entry.stock}</td>
+                                <td>{(entry.stock - entry.quantity) >= 0 ? "-" : (entry.quantity - entry.stock)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <a ref={downloadButtonRef} href="">Download as CSV</a>
+                <a ref={downloadApproButtonRef} href="">Download CSV Appro</a>
                 <button
                     onClick={closeModal}
                 >Close</button>
